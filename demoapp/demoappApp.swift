f507ae6623ecdb8c7f7f4718b58fe4e61d01454e
jordaeni13 <bhc@jeju-s.jje.hs.kr>
2023-05-06 19:57:02 +0900

    Initial commit

---
+++ demoapp/demoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoappApp.swift
+//  demoapp
+//
+//  Created by Iron Bae on 2023/05/06.
+//
+
+import SwiftUI
+
+@main
+struct demoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
