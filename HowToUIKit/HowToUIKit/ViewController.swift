3dff3109f82ea468681feffebb4a9ce3d20b61b1
jordaeni13 <bhc@jeju-s.jje.hs.kr>
2023-05-07 20:39:39 +0900

    stuffs that I've done today

---
+++ HowToUIKit/HowToUIKit/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HowToUIKit
+//
+//  Created by Iron Bae on 2023/05/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var cardView: UIView!
+    @IBOutlet var blurView: UIVisualEffectView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
