36236e029c5dc027393785997f4d87af1ff87f4b
Wooyoung Kang <wkang0211@gmail.com>
2023-05-29 15:58:19 +0900

    Update CameraView.swift

--- ARCHIVE_SWIFTUI_Hapchin/demoapp/Views/CameraView.swift
+++ ARCHIVE_SWIFTUI_Hapchin/demoapp/Views/CameraView.swift
@@ -11,11 +11,31 @@ struct CameraView: View {
     
     @State private var tapCount = 0
     var body: some View {
-        Text("Tap Count: \(tapCount)")
-                    .onTapGesture(count: 2){
-                        tapCount+=1
+        ZStack {
+            Color.black
+                .ignoresSafeArea(.all, edges: .all)
+        }
+        VStack {
+            Spacer()
+            
+            HStack{
+                Button(action: {}, label: {
+                    ZStack{
+                        Circle()
+                            .fill(Color.white)
+                            .frame(width: 65, height: 65)
+                        Circle()
+                            .stroke(Color.white, lineWidth: 2)
+                            .frame(width: 75, height: 75)
                     }
+                })
+            }
+        }
     }
+}
+
+class CameraModel: ObservedObject{
+    @Published var isTaken = false
 }
 
 struct CameraView_Previews: PreviewProvider {
